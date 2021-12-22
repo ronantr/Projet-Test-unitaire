77dff632fb423cd552614b0759a26ba83253f7c4
pthushanth <pthushanth97@gmail.com>
2021-12-22 22:07:34 +0100

    Add new testcase for the user entity

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -6,19 +6,74 @@ namespace tests\AppBundle\Entity;
 use App\Entity\User as EntityUser;
 use AppBundle\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Carbon\Carbon;
 
 class UserTest extends TestCase
 {
 
-    public function testUserCreate()
-    {
-        $user = new EntityUser(); // Create User object.
+    private EntityUser $user;
 
-        $user->setFirstname("firstname");
-        $user->setLastname("lastname");
-        $user->setBirthday("1997-10-10");
-        $user->setEmail("test@gmail.com");
-        $user->setPassword("12345678");
-        $this->assertTrue($user->isValid());
+    protected function setUp(): void
+    {
+        $this->user = new EntityUser(); // Create User object.
+
+        $this->user->setFirstname("firstname");
+        $this->user->setLastname("lastname");
+        $this->user->setBirthday(Carbon::now()->subYears(20));
+        $this->user->setEmail("test@gmail.com");
+        $this->user->setPassword("12345678");
+        parent::setUp();
+    }
+
+    public function testValidUser()
+    {
+        $this->assertTrue($this->user->isValid());
+    }
+
+    public function testEmptyEmail()
+    {
+        $this->user->setEmail("");
+
+        $this->assertFalse($this->user->isValid());
+    }
+
+    public function testInvalidEmailFormat()
+    {
+        $this->user->setEmail("testgmail.com");
+        $this->assertFalse($this->user->isValid());
+    }
+
+    public function testEmptyFirstname()
+    {
+        $this->user->setFirstname("");
+        $this->assertFalse($this->user->isValid());
+    }
+    public function testEmptyLastname()
+    {
+        $this->user->setLastname("");
+        $this->assertFalse($this->user->isValid());
+    }
+    public function testEmptyPassword()
+    {
+        $this->user->setPassword("");
+        $this->assertFalse($this->user->isValid());
+    }
+
+    public function testShortPassword()
+    {
+        $this->user->setPassword("1234567");
+        $this->assertFalse($this->user->isValid());
+    }
+    public function testLongPassword()
+    {
+        //password length 41
+        $this->user->setPassword("3lfFGf3OQIaRfmDFfQMJ2RzmmpC7d5B6eGzKJqmnX");
+        $this->assertFalse($this->user->isValid());
+    }
+
+    public function testAgeTooYoung()
+    {
+        $this->user->setBirthday(Carbon::now()->subDecade());
+        $this->assertFalse($this->user->isValid());
     }
 }
